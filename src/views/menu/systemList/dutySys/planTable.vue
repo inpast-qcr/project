ec4cbcb5ac1f5f9db4c2ffd2ee9c2d1d916e63cd
inpast-qcr <inpast_qcr@163.com>
2024-05-22 15:24:23 +0800

    值日排班设置head

---
+++ src/views/menu/systemList/dutySys/planTable.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="home">
+        <!-- header -->
+        <div class="title">
+            <div class="titleLogo">值日排班设置</div>
+            <div class="titleBack">
+                <el-button class="backMenuButton" style="margin-right: 30px;" type="primary" @click="backMenu" icon="el-icon-arrow-left">返回应用中心</el-button>
+            </div>
+        </div>
+
+        <div class="planTable"></div>
+    </div>
+</template>
+  
+<script>
+  
+  export default{
+    methods:{
+
+    }
+  }
+
+</script>
+  
+<style lang="less" scoped>
+.home{
+    .title{
+        display: flex;
+        width: 100%;
+        height: 40px;
+        
+        .titleLogo{
+            flex: 1;
+            text-align: left;
+            font-size: 20px;
+            font-weight: bold;
+            height: 38px;
+            
+        }
+    }
+}
+</style>
