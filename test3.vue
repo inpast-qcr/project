59142564d54b87d63881935b0fad9f77d1f26edb
inpast-qcr <inpast_qcr@163.com>
2024-05-28 14:18:54 +0800

    测试

---
+++ test3.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+        测试
+    </div>
+</template>
+  
+<script>
+  
+export default{
+    name:'',
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}
+
+</script>
+  
+<style lang="less" scoped>
+.home{
+
+}
+</style>
